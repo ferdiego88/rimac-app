2070d27b5b3f52baf608a988919c34cdc8fe589f
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-02-26 03:33:53 -0500

    update config package

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import('next').NextConfig}
- */
-
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
-const withFonts = require('./next.config.common');
-export default withFonts;
